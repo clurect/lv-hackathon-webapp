2ad45ee46f011527be1490eaf49e268a85935a3e
Keith Brown <kbrown@longviewinc.com>
2015-05-21 13:09:41 -0400

    basic layout

--- app/js/controller/baseController.js
+++ app/js/controller/baseController.js
@@ -1,7 +1,6 @@
 define(function (require) {
   var App = require('app');
   var HeaderView = require('view/header');
-  var FooterView = require('view/footer');
   var MainView = require('view/main');
 
   return Backbone.Marionette.Controller.extend({
@@ -10,8 +9,8 @@ define(function (require) {
       App.views = {};
       App.views.main = new MainView();
       App.views.header = new HeaderView();
-      App.views.footer = new FooterView();
       App.containerRegion.show(App.views.main);
+      App.views.main.headerRegion.show(App.views.header);
     },
     showIndex: function () {
       
--- app/js/view/main.js
+++ app/js/view/main.js
@@ -2,12 +2,15 @@ define(function (require) {
   var App = require('app');
   var template = require('text!../../html/main.html');
 
-  return Backbone.Marionette.ItemView.extend({
+  return Backbone.Marionette.Layout.extend({
     className: '',
     template: _.template(template),
-
+    regions: {
+      'headerRegion': '.header',
+      'postListRegion': '.post-list'
+    },
     events: {
-
+      
     }
 
   });
